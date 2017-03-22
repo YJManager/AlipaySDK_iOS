1351fc59ecaf671ec6034f0f89527cb69313d72a
yuejun.hou <8069060@qq.com>
2017-03-22 15:30:03 +0800

    626

---
+++ AlipaySDKDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AlipaySDKDemo
+//
+//  Created by YJHou on 2017/3/22.
+//  Copyright © 2017年 Houmanager. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
